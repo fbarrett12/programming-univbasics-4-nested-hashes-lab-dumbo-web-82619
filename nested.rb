c0a8192e0ab4eb6c71ec26f8532458f2d1b68cdc
Fredrick Barrett <f.barrett12@gmail.com>
2019-08-24 05:52:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
           :languages => ["C"]
         }
      }
-
+ programmer_hash[:grace_hopper]
 
 end
 
@@ -37,9 +37,11 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
+  
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -55,6 +57,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+     programmer_hash[:dennis_ritchie][:languages][0]
 end
 
 def adding_matz
@@ -79,7 +82,7 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
+programmer_hash[:yukihiro_matsumoto]
     
 end
 
